4dce6256d5498a2ad601f422f737f989d86e6b14
Mohammed Atef <mohamed45452020@gmail.com>
2024-09-29 22:39:55 +0300

    Initial Version

---
+++ A1_T1_20231143.cpp
@@ -0,0 +1,15 @@
+// File: A1_T1_20231143.cpp
+// Purpose: .........
+// Author: Mohammed Atef Abd El-Kader
+// ID: 20231143
+// TA:
+// Date: 8 Oct 2024
+// Version: 1.0
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
